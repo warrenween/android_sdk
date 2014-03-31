ca3e2f3bfea69f529b10c6dfd118e0aebb1cfa3a
Bjorn Roche <bjorn@xowave.com>
2014-03-31 14:31:12 -0400

    user agent.

--- sdk/src/com/chartbeat/androidsdk/Pinger.java
+++ sdk/src/com/chartbeat/androidsdk/Pinger.java
@@ -36,81 +36,97 @@ final class Pinger {
 	private DefaultHttpClient httpClient = new DefaultHttpClient();
 	
 	private final Random random = new Random(0); //used only for testing
-
+	
 	/**
 	 * 
 	 */
 	Pinger(String userAgent) {
-		httpClient.getParams().setParameter(CoreProtocolPNames.USER_AGENT, "Custom user agent");
+		httpClient.getParams().setParameter(CoreProtocolPNames.USER_AGENT, userAgent);
 	}
 	
 	/** returns true on success, false if the server wants us to wait, and an exception in any other case.
 	 * */
 	int ping(List<KeyValuePair> parameters) throws IOException {
 	    HttpResponse response;
-		while( true ) {
-			// setup the call
-			Uri.Builder builder = new Uri.Builder().scheme(SCHEME).authority(AUTHORITY).path(PATH);
-			for( KeyValuePair e : parameters )
-				builder.appendQueryParameter(e.getKey(), e.getValue());
-			HttpGet get = new HttpGet( builder.build().toString() );
-			if( Tracker.DEBUG )
-				Log.d(TAG,"Ping URL: " + get.getURI().toString());
-			
-			// execute the call
-			response = httpClient.execute(get);
-		    StatusLine statusLine = response.getStatusLine();
-		    int code = statusLine.getStatusCode();
-			if( Tracker.DEBUG && ( code < 200 || code >= 300 ) ) {
-				Log.d(TAG,"-----Error. Response from server:" );
-				Log.d(TAG,EntityUtils.toString(response.getEntity()) );
-				Log.d(TAG,"-----" );
-			} else {
-				response.getEntity().consumeContent();
-			}
-			
-			// results:
-		    switch( code ) {
-		    case 200:
-		    	if( TEST_RANDOM_FAILURES ) {
-		    		int r = random.nextInt(6);
-		    		if( r == 0 ) {
-		    			Log.w(TAG, "Simulating a fake 400 response." );
-		    			return 400;
-		    		}
-		    		if( r > 2 ) {
-		    			Log.w(TAG, "Simulating a fake 503 response." );
-		    			return 503;
-		    		}
-		    	}
-		    case 500:
-		    case 503:
-		    	return code;
-		    default:
-		    	//standard responses:
-		    	if( code < 200 || code >= 600 ) {
-		    		throw new IOException("Unkown response code: " + code);
-		    	}
-		    	if( code >= 200 && code < 300 ) {
-		    		//this is considered a success, but we'll log it b/c it's not documented.
-		    		Log.w(TAG, "Unexpected success response code: " + code );
-		    		return 200;
-		    	}
-		    	if( code >= 300 && code < 400 ) {
-		    		//misc redirect. should have been handled by httpclient, since this is a GET
-		    		throw new IOException("HttpClient did not handle redirect: " + code );
-		    	}
-		    	if( code >= 400 && code < 500 ) {
-		    		//client error.
-		    		throw new IOException("HttpClient error: " + code );
-		    	}
-		    	if( code >= 500 && code < 600 ) {
-		    		Log.w(TAG, "Unexpected server response code: " + code );
-		    		return 503; // retry later
-		    	}
-		    	throw new IOException("Unknown error: " + code );
-		    }
+		/*
+		{
+		Uri.Builder builder = new Uri.Builder().scheme("http").authority("whatsmyuseragent.com").path("/");
+		for( KeyValuePair e : parameters )
+			builder.appendQueryParameter(e.getKey(), e.getValue());
+		HttpGet get = new HttpGet( builder.build().toString() );
+		if( Tracker.DEBUG )
+			Log.d(TAG,"Ping URL: " + get.getURI().toString());
+		
+		// execute the call
+		response = httpClient.execute(get);
+		Log.d(TAG,"-----UserAgent response:" );
+		Log.d(TAG,EntityUtils.toString(response.getEntity()) );
+		Log.d(TAG,"-----" );
 		}
+		*/
+		
+		
+		// setup the call
+		Uri.Builder builder = new Uri.Builder().scheme(SCHEME).authority(AUTHORITY).path(PATH);
+		for( KeyValuePair e : parameters )
+			builder.appendQueryParameter(e.getKey(), e.getValue());
+		HttpGet get = new HttpGet( builder.build().toString() );
+		if( Tracker.DEBUG )
+			Log.d(TAG,"Ping URL: " + get.getURI().toString());
+		
+		// execute the call
+		response = httpClient.execute(get);
+	    StatusLine statusLine = response.getStatusLine();
+	    int code = statusLine.getStatusCode();
+		if( Tracker.DEBUG && ( code < 200 || code >= 300 ) ) {
+			Log.d(TAG,"-----Error. Response from server:" );
+			Log.d(TAG,EntityUtils.toString(response.getEntity()) );
+			Log.d(TAG,"-----" );
+		} else {
+			response.getEntity().consumeContent();
+		}
+		
+		// results:
+	    switch( code ) {
+	    case 200:
+	    	if( TEST_RANDOM_FAILURES ) {
+	    		int r = random.nextInt(6);
+	    		if( r == 0 ) {
+	    			Log.w(TAG, "Simulating a fake 400 response." );
+	    			return 400;
+	    		}
+	    		if( r > 2 ) {
+	    			Log.w(TAG, "Simulating a fake 503 response." );
+	    			return 503;
+	    		}
+	    	}
+	    case 500:
+	    case 503:
+	    	return code;
+	    default:
+	    	//standard responses:
+	    	if( code < 200 || code >= 600 ) {
+	    		throw new IOException("Unkown response code: " + code);
+	    	}
+	    	if( code >= 200 && code < 300 ) {
+	    		//this is considered a success, but we'll log it b/c it's not documented.
+	    		Log.w(TAG, "Unexpected success response code: " + code );
+	    		return 200;
+	    	}
+	    	if( code >= 300 && code < 400 ) {
+	    		//misc redirect. should have been handled by httpclient, since this is a GET
+	    		throw new IOException("HttpClient did not handle redirect: " + code );
+	    	}
+	    	if( code >= 400 && code < 500 ) {
+	    		//client error.
+	    		throw new IOException("HttpClient error: " + code );
+	    	}
+	    	if( code >= 500 && code < 600 ) {
+	    		Log.w(TAG, "Unexpected server response code: " + code );
+	    		return 503; // retry later
+	    	}
+	    	throw new IOException("Unknown error: " + code );
+	    }
 	}
 	
 	static boolean isConnected(Context context)
